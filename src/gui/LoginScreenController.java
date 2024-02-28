8a81c74a6c3ee4b7624c3a67643de19b59afff23
Quinten <quinten.deconinck@student.hogent.be>
2024-02-28 14:16:00 +0100

    Start companies scherm

--- src/gui/LoginScreenController.java
+++ src/gui/LoginScreenController.java
@@ -1,11 +1,27 @@
 package gui;
 
 import domain.DomainController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
-public class LoginScreenController {
+public class LoginScreenController extends Pane {
+	
+	 private DomainController dc;
+	 private HBox hb = new HBox();
 
 	public LoginScreenController(DomainController dc) {
-		// TODO Auto-generated constructor stub
+		this.dc = dc;
+		buildGUI();
 	}
+
+	private void buildGUI() {
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("SpelOverzicht.fxml"));
+    	loader.setController(this);
+    	loader.setRoot(this);
+		
+	}
+	
+	
 
 }
