1e27b0ef01b7eb7f6412d6d91d17468289e6963d
Arno Heekhout <114562521+arno-heekhout@users.noreply.github.com>
2024-03-10 18:21:13 +0100

    Manier om dev en prod env te hebben

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -14,15 +14,31 @@ public class StartUp extends Application {
 	public static void main(String[] args) throws Exception {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage stage) throws IOException {
+		if (isDevelopmentEnvironment()) {
+			initializeDevelopmentEnvironment(); // Seeden
+		}
+
 		DomainController dc = new DomainController();
-		
+
 		AdminScreenController root = new AdminScreenController(dc);
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.setTitle("SDP2_G16");
 		stage.show();
+	}
+
+	private boolean isDevelopmentEnvironment() {
+		// Kijk of --dev is meegegeven bij de startup opties
+		// https://docs.oracle.com/javase/8/javafx/api/javafx/application/Application.Parameters.html
+		return getParameters().getRaw().contains("--dev");
+
+	}
+
+	private void initializeDevelopmentEnvironment() throws IOException {
+		// Roep seeding aan
+		System.out.println("Seeding");
 	}
 }
