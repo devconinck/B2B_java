54db2a33abb7ae86ad944a1347a17794b6887f74
Arno Heekhout <114562521+arno-heekhout@users.noreply.github.com>
2024-03-20 14:44:41 +0100

    Filter toevoegen, onnodige fxml weg

--- src/gui/order/OrdersFilterController.java
+++ src/gui/order/OrdersFilterController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import domain.Order;
+import gui.FilterController;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -12,43 +13,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
-public class OrdersFilterController extends HBox {
+public class OrdersFilterController extends FilterController<Order> {
+
+	public OrdersFilterController(ObservableList<Order> originalList) {
+		super(originalList);
+	}
+
+	@Override
+	public List<Order> Filter(List<Order> list) {
+		return list.stream()
+                .filter(o -> o.getOrderId().contains(super.getSearchText()))
+                .collect(Collectors.toList());
+	}
 	
-	private final TextField filterField;
-    private final ObservableList<Order> originalList;
-    private final ObservableList<Order> filteredList;
-
-    public OrdersFilterController(ObservableList<Order> originalList) {
-        this.originalList = originalList;
-        this.filteredList = FXCollections.observableArrayList();
-        filterField = new TextField();
-        filterField.setPromptText("Search by order id");
-        
-        HBox.setHgrow(filterField, Priority.ALWAYS);
-        
-        filterField.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                // Call the filter method whenever the text changes
-                getFilteredList(originalList);
-            }
-        });
-        
-        getChildren().add(filterField);
-    }
-    
-
-    public ObservableList<Order> getFilteredList(List<Order> orderList) {
-        String searchText = filterField.getText().trim();
-        if (searchText.isEmpty()) {
-            filteredList.setAll(originalList);
-        } else {
-            List<Order> filtered = orderList.stream()
-                                                .filter(o -> o.getOrderId().contains(searchText))
-                                                .collect(Collectors.toList());
-            filteredList.setAll(filtered);
-        }
-        return filteredList;
-    }
-
 }
