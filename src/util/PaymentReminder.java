fc28103a296288f88361d915ac23252e4d5eebec
Arno Heekhout <114562521+arno-heekhout@users.noreply.github.com>
2024-03-24 14:29:48 +0100

    Test erbij gezet

--- src/util/PaymentReminder.java
+++ src/util/PaymentReminder.java
@@ -10,8 +10,6 @@ import mail.SendMail;
 import repository.GenericDaoJpa;
 
 public class PaymentReminder {
-
-	// Haal alle order op met orderDate + payment period <= 3?
 	private B2BPortaal portaal;
 	private SendMail mail;
 	
@@ -20,7 +18,6 @@ public class PaymentReminder {
 		this.mail = new SendMail();
 		sendPaymentReminders();
 	}
-	
 	
 	public void sendPaymentReminders() {
 	    LocalDate currentDate = LocalDate.now();
@@ -32,22 +29,59 @@ public class PaymentReminder {
 	        if (currentDate.isEqual(paymentDueDate.minusDays(3)) && 
 	        		(!order.getPaymentStatus().equals(PaymentStatus.PAID))) {
 	            Company tempComp = order.getCompany();
-	            // Moet nog is volledig getest worden
-	            /*
+
 	            mail.sendMail(tempComp.getContact().getEmail(), sender,
-						String.format("Payment due to %s", order.getOrderDateTime().plusDays(Validation.PAYMENT_PERIOD)),
+						String.format("Payment due to %s", paymentDueDate),
 						String.format(
 								"You have a order that is not been payed yet.%nThis order has id: %s%nYours Sincerely %n%s",
 								order.getOrderID(), tempComp.getName()));
-				*/
+				
 	        }
 	    });
-	    
+	   
 	    // Nuttig?
 	    portaal = null;
 	    mail = null;
 	    System.gc();
 	}
 	
+	// TODO
+	// Moet nog eens volledig gerunt worden, maar denk wel dat het werkt
+	/*
+	int count = 0;
 	
+	public void sendPaymentReminders() {
+	    LocalDate currentDate = LocalDate.now();
+	    String sender = "sdpgroep@gmail.com";
+	    
+	    
+	    
+	    portaal.getOrdersList().forEach(order -> {
+	    	
+	        LocalDate paymentDueDate = order.getOrderDateTime().plusDays(Validation.PAYMENT_PERIOD);
+	        
+	        if (currentDate.isEqual(paymentDueDate.minusDays(3)) && 
+	        		(!order.getPaymentStatus().equals(PaymentStatus.PAID))) {
+	            Company tempComp = order.getCompany();
+	            // Moet nog is volledig getest worden
+	            
+	            System.out.printf("%s %s ", order.getOrderDateTime(), order.getOrderID());
+	            count += 1;
+	            mail.sendMail(tempComp.getContact().getEmail(), sender,
+						String.format("Payment due to %s", order.getOrderDateTime().plusDays(Validation.PAYMENT_PERIOD)),
+						String.format(
+								"You have a order that is not been payed yet.%nThis order has id: %s%nYours Sincerely %n%s",
+								order.getOrderID(), tempComp.getName()));
+				
+	        }
+	    });
+	    
+	    System.out.println(count);
+	    
+	    // Nuttig?
+	    portaal = null;
+	    mail = null;
+	    System.gc();
+	}
+	*/
 }
